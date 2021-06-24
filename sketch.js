d7b26b1f45574a577b7f80451ff100c4096d7450
TopprCodr <72185815+TopprCodr@users.noreply.github.com>
2021-06-24 18:29:54 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+var Engine = Matter.Engine,
+    Render = Matter.Render,
+    Runner = Matter.Runner,
+    Composites = Matter.Composites,
+    Common = Matter.Common,
+    MouseConstraint = Matter.MouseConstraint,
+    Mouse = Matter.Mouse,
+    Composite = Matter.Composite,
+    Bodies = Matter.Bodies;
+
+// create engine
+var engine = Engine.create(),
+    world = engine.world;
+
+// create renderer
+var render = Render.create({
+    element: document.body,
+    engine: engine,
+    options: {
+        width: 800,
+        height: 600,
+        wireframes: false,
+        background: 'rgb(0,100,25)'
+    }
+});
+
+Render.run(render);
+
+// create runner
+var runner = Runner.create();
+Runner.run(runner, engine);
+
